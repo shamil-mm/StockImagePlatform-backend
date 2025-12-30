fa8168c6357df1242c762ee9325b3a956fc80825
shamil <shamil880699@gmail.com>
2025-12-31 01:10:07 +0530

    checking cors issue solved 0.7

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,10 @@ app.use(
   cors({
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
-
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
-
-      return callback(new Error("Not allowed by CORS"), false);
+      return callback(null, false);
     },
     credentials: true,
   })
