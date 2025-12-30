4c6a0e841fd7dd7760ebaeff8f094a1bed9baf62
shamil <shamil880699@gmail.com>
2025-12-31 00:50:13 +0530

    checking cors issue solved 0.3

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,23 @@ import AuthRoutes from './Routes/authRoutes'
 
 
 const app = express();
+const allowedOrigins = [
+  "https://stock-image-platform-frontend-blush.vercel.app",
+  "https://stock-image-platform-frontend-git-main-shamil-m-ms-projects.vercel.app",
+  "https://stock-image-platform-frontend-l3jdgwfup-shamil-m-ms-projects.vercel.app",
+];
 
 app.use(cors({
-  origin:process.env.VITE_FRONTEND_URI,
-  credentials:true,
-}));
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"), false);
+    },
+    credentials: true,
+  }));
 app.use(express.json());
 
 app.use("/api/auth",AuthRoutes)
